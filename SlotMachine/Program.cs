e9ff5ee10dd21df2b8bb5c28dbf607a467299c6f
kirannandedkar <kirannandedkar@yahoo.com>
2023-02-03 15:29:27 +0100

    Wrote logic for center line

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -16,18 +16,53 @@ namespace SlotMachine
                               "\n Each line cost 2 dollars. So if you select 5 then it will cost you 10 dollars");
             string input = Console.ReadLine();
             int[,] slotMachineArray = new int[3, 3];
-            
-            Random rnd = new Random();
-            var s = slotMachineArray.GetLength(0);
-            for (int i = 0; i < slotMachineArray.GetLength(0); i++)
-            {
-                for (int j = 0; j < slotMachineArray.GetLength(0); j++)
-                {
-                    slotMachineArray[i,j] = rnd.Next(0, 2);
-                }
-            }
-            
 
+            bool continueToPlay = false;
+            int moneyWon = 0;
+
+            do
+            {
+                Random rnd = new Random();
+
+                for (int i = 0; i < slotMachineArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < slotMachineArray.GetLength(1); j++)
+                    {
+                        slotMachineArray[i, j] = rnd.Next(0, 2);
+                    }
+                }
+
+                if (input == "1") //center line
+                {
+                    if (slotMachineArray[1, 0] == slotMachineArray[1, 1] &&
+                        slotMachineArray[1, 1] == slotMachineArray[1, 2])
+                    {
+                        Console.WriteLine("Congrats you won");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad Luck you lost");
+                    }
+
+
+                }
+
+                if (input == "2")
+                {
+
+                }
+
+                Console.WriteLine("Press Y to continue to play");
+                string playAgain = Console.ReadLine();
+                if (playAgain.ToLower() == "y")
+                {
+                    continueToPlay = true;
+                }
+
+
+
+            } while (continueToPlay);
+            
 
         }
     }
