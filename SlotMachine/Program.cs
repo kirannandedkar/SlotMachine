d356d94be353d01423e08d967cac2fe25a117959
kirannandedkar <kirannandedkar@yahoo.com>
2023-02-08 22:06:06 +0100

    Finished the logic where player selects all lines

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -1,72 +1,192 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace SlotMachine
 {
     internal class Program
     {
-        const double MONEY_AMOUNT = 2;
+        const double WIN_MONEY_AMOUNT_PER_LINE = 2;
+        static double moneyWon = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Slot Machine Game");
             Console.WriteLine("Here are few rules on how to play the game");
-            Console.WriteLine("Press 1 if you want to play center line" +
-                              "\nPress 2 if you want to play horizontal lines" +
-                              "\nPress 3 if you want to play vertical lines" +
-                              "\nPress 4 if you want to play diagonal line" +
-                              "\nPress 5 if you want to play all lines" +
-                              "\n Each line cost 2 dollars. So if you select 5 then it will cost you 10 dollars");
-           
+            
             int[,] slotMachineArray = new int[3, 3];
 
             bool continueToPlay = false;
-            double moneyWon = 0;
 
             do
             {
-                string input = Console.ReadLine();
+                Console.WriteLine("Press 1 if you want to play center line/middle horizontal line" +
+                                  "\nPress 2 if you want to play top horizontal line" +
+                                  "\nPress 3 if you want to play bottom horizontal line" +
+                                  "\nPress 4 if you want to play left vertical line" +
+                                  "\nPress 5 if you want to play middle vertical line" +
+                                  "\nPress 6 if you want to play right vertical line" +
+                                  "\nPress 7 if you want to play diagonal line" +
+                                  "\nPress 8 if you want to play all lines" +
+                                  "\n Each line cost 1 dollar. So if you select 5 then it will cost you 5 dollars" +
+                                  "\n For each winning line you win 2 dollars. So if you win more than 1 line you get no of lines * 2 dollars back" +
+                                  "\n Please enter your choice of line you want to play");
+
                 Random rnd = new Random();
 
+                string input = Console.ReadLine();
                 for (int i = 0; i < slotMachineArray.GetLength(0); i++)
                 {
                     for (int j = 0; j < slotMachineArray.GetLength(1); j++)
                     {
                         slotMachineArray[i, j] = rnd.Next(0, 2);
-                        
+
                     }
                 }
                 OutputArray(slotMachineArray);
-
-                if (input == "1") //center line
+                
+                if (input == "1") //center line and middle horizontal row
                 {
                     if (slotMachineArray[1, 0] == slotMachineArray[1, 1] &&
                         slotMachineArray[1, 1] == slotMachineArray[1, 2])
-                        
                     {
-                        Console.WriteLine("Congrats you won");
-                        moneyWon += MONEY_AMOUNT;
+                        DisplayWinAmount();
                     }
                     else
                     {
-                        Console.WriteLine("Bad Luck you lost");
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
                     }
-
-
                 }
 
-                if (input == "2")
+                if (input == "2") //Top horizontal row
                 {
-                    if ((slotMachineArray[0, 0] == slotMachineArray[0, 1] && slotMachineArray[0, 1] == slotMachineArray[0, 2]) ||
-                        (slotMachineArray[1, 0] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[1, 2]) ||
-                        (slotMachineArray[2, 0] == slotMachineArray[2, 1] && slotMachineArray[2, 1] == slotMachineArray[2, 2]))
+                    if ((slotMachineArray[0, 0] == slotMachineArray[0, 1] && slotMachineArray[0, 1] == slotMachineArray[0, 2]))
                     {
-                        Console.WriteLine("Congrats you won");
-                        moneyWon += MONEY_AMOUNT;
+                        DisplayWinAmount();
                     }
                     else
                     {
-                        Console.WriteLine("Bad Luck you lost");
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
+                }
+
+                if (input == "3") //bottom horizontal row
+                {
+                    if ((slotMachineArray[2, 0] == slotMachineArray[2, 1] && slotMachineArray[2, 1] == slotMachineArray[2, 2]))
+                    {
+                        DisplayWinAmount();
+                    }
+                    else
+                    {
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
+                }
+
+                if (input == "4") //left vertical row
+                {
+                    if ((slotMachineArray[0, 0] == slotMachineArray[1, 0] && slotMachineArray[1, 0] == slotMachineArray[2, 0]))
+                    {
+                        DisplayWinAmount();
+                    }
+                    else
+                    {
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
+                }
+
+                if (input == "5") //middle vertical row
+                {
+                    if ((slotMachineArray[0, 1] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[2, 1]))
+                    {
+                        DisplayWinAmount();
+                    }
+                    else
+                    {
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
+                }
+
+                if (input == "6") //right vertical row
+                {
+                    if ((slotMachineArray[0, 2] == slotMachineArray[1, 2] && slotMachineArray[1, 2] == slotMachineArray[2, 2]))
+                    {
+                        DisplayWinAmount();
+                    }
+                    else
+                    {
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
+                }
+
+                if (input == "7") //diagonal line
+                {
+                    if ((slotMachineArray[2, 0] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[0, 2]) ||
+                        (slotMachineArray[0, 0] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[2, 2]))
+                    {
+                        DisplayWinAmount();
+
+                    }
+                    else
+                    {
+                        moneyWon -= 1;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
+                }
+
+                if (input == "8")
+                {
+                    int noOfLinesWon = 0;
+
+                    if (slotMachineArray[0, 0] == slotMachineArray[0, 1] && slotMachineArray[0, 1] == slotMachineArray[0, 2]) //Top horizontal row
+                    {
+                        noOfLinesWon += 1;
                     }
 
+                    if ((slotMachineArray[1, 0] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[1, 2]))  //center line and middle horizontal row
+                    {
+                        noOfLinesWon += 1;
+                    }
+
+                    if (slotMachineArray[2, 0] == slotMachineArray[2, 1] && slotMachineArray[2, 1] == slotMachineArray[2, 2])  //bottom horizontal
+                    {
+                        noOfLinesWon += 1;
+                    }
+
+                    if ((slotMachineArray[0, 0] == slotMachineArray[1, 0] && slotMachineArray[1, 0] == slotMachineArray[2, 0])) //left vertical row
+                    {
+                        noOfLinesWon += 1;
+                    }
+
+                    if ((slotMachineArray[0, 1] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[2, 1])) //middle vertical row
+                    {
+                        noOfLinesWon += 1;
+                    }
+
+                    if ((slotMachineArray[0, 2] == slotMachineArray[1, 2] && slotMachineArray[1, 2] == slotMachineArray[2, 2])) //right vertical row
+                    {
+                        noOfLinesWon += 1;
+                    }
+
+                    if (slotMachineArray[2, 0] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[0, 2])  //diagonal line
+                    {
+                        noOfLinesWon += 1;
+                    } 
+                    
+                    if (slotMachineArray[0, 0] == slotMachineArray[1, 1] && slotMachineArray[1, 1] == slotMachineArray[2, 2])  //diagonal line
+                    {
+                        noOfLinesWon += 1;
+                    }
+
+                    DisplayWinAmount(noOfLinesWon);
+                    if (noOfLinesWon == 0)
+                    {
+                        moneyWon -= 1*8;
+                        Console.WriteLine($"Bad Luck you lost. Now you have {moneyWon} USD amount remaining");
+                    }
                 }
 
                 Console.WriteLine("Press Y to continue to play");
@@ -74,10 +194,11 @@ namespace SlotMachine
                 if (playAgain.ToLower() == "y")
                 {
                     continueToPlay = true;
+                    Console.Clear();
                     Console.WriteLine("Please select options again");
                 }
 
-
+                Console.WriteLine($"Amount won so far is {moneyWon} USD");
 
             } while (continueToPlay);
             
@@ -95,6 +216,12 @@ namespace SlotMachine
 
                 Console.WriteLine("\n");
             }
+        }
+
+        static void DisplayWinAmount(int noOfLinesWon = 1)
+        {
+            moneyWon += WIN_MONEY_AMOUNT_PER_LINE * noOfLinesWon;
+            Console.WriteLine($"Congrats you won. You have total {moneyWon} dollars");
         }
     }
 }
