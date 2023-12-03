50893d5cd0434bbdc998a627f2c3f122d64e8975
kirannandedkar <kirannandedkar@yahoo.com>
2023-12-03 12:55:56 +0100

    #19  removed unnecessary readinput

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -41,7 +41,6 @@ namespace SlotMachine
                     else if (selectedOption == 8 && TOTAL_CREDIT < 8)
                     {
                         Console.WriteLine("Sorry you dont have enough credit left. select different input");
-                        isInputValid = int.TryParse(Console.ReadLine(), out selectedOption);
                         isInputValid = false;
                     }
 
